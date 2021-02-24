5ff2a2621318884a835313f4438404fe3e03b26e
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-23 20:39:27 -0800

    Adjustments and additions
    Improve HTML formatting, add button for possible 'clear-all', correct time display, add function for colorcoding

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-$("#currentDay").text(moment().format('MMMM Do, YYYY | h:mm:ss a'));
+// displays current date and time when page loads
+
+$("#currentDay").text(moment().format('MMMM Do, YYYY | h:mm a'));
+
+// create function to check time and compare with 'time-block' -- change colors accordingly
+
+var currentTime = moment();
+
+$(".time-block").each(function colorCode() {
+    var hourSlot = parseInt($(this).attr("id"));
+    if (hourSlot < currentTime) {
+        $(this).addClass("past");
+      } 
+      else if (hourSlot === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    }
+})
+
