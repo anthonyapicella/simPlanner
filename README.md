0bc264add3c898d1959a7bbc881a36b1e7809eca
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-02-23 22:23:28 -0800

    Add JS to load previously saved data from local storage

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simPlanner
+
+## A simple calendar app for scheduling your werk day
+
+* A fully responsive web application that allows the user to schedule their work day (9AM-5PM)
--- assets/script.js
+++ assets/script.js
@@ -33,4 +33,15 @@ $(".saveBtn").on("click", function() {
 });
 
 
-// pull any previously saved entries and display on page load...
+// using query selector, pull any previously saved entries from local storage and display on page load
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("7"));
+
